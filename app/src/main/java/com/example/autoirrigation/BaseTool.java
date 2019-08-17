69cd89092af231d7b24a34a403c3a74b046e735a
limbo <qianchaosolo@gmail.com>
2019-08-17 20:59:33 +0800

    Set status transparent

--- app/src/main/java/com/example/autoirrigation/BaseTool.java
+++ app/src/main/java/com/example/autoirrigation/BaseTool.java
@@ -5,7 +5,7 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-class BaseTool {
+public class BaseTool {
     static void setStatusTransparent(Window window) {
         window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
